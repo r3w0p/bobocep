f841a4ec6a279c0f22cb1c4f8a4fe953f5a10c36
r3w0p <23155844+r3w0p@users.noreply.github.com>
2021-01-25 18:17:42 +0000

    more callable predict tests

--- tests/test_rules/test_predicates/test_predicate_callable.py
+++ tests/test_rules/test_predicates/test_predicate_callable.py
@@ -64,3 +64,66 @@ def test_callable_valid_method():
     predicate = BoboPredicateCallable(call=call)
 
     assert predicate.evaluate(test_event, test_history)
+
+
+def test_callable_invalid_callable():
+    call = 123456
+
+    with pytest.raises(PreconditionError):
+        BoboPredicateCallable(call=call)
+
+
+def test_callable_invalid_function_too_few_parameters():
+    def invalid_callable_function(p):
+        return True
+
+    call = invalid_callable_function
+
+    with pytest.raises(RuntimeError):
+        BoboPredicateCallable(call=call)
+
+
+def test_callable_invalid_lambda_too_few_parameters():
+    call = lambda p: True
+
+    with pytest.raises(RuntimeError):
+        BoboPredicateCallable(call=call)
+
+
+def test_callable_invalid_method_too_few_parameters():
+    class TestInvalidClass:
+        def evaluate(self, p) -> bool:
+            return True
+
+    call = TestInvalidClass().evaluate
+
+    with pytest.raises(RuntimeError):
+        BoboPredicateCallable(call=call)
+
+
+def test_callable_invalid_function_too_many_parameters():
+    def invalid_callable_function(p1, p2, p3):
+        return True
+
+    call = invalid_callable_function
+
+    with pytest.raises(RuntimeError):
+        BoboPredicateCallable(call=call)
+
+
+def test_callable_invalid_lambda_too_many_parameters():
+    call = lambda p1, p2, p3: True
+
+    with pytest.raises(RuntimeError):
+        BoboPredicateCallable(call=call)
+
+
+def test_callable_invalid_method_too_many_parameters():
+    class TestInvalidClass:
+        def evaluate(self, p1, p2, p3) -> bool:
+            return True
+
+    call = TestInvalidClass().evaluate
+
+    with pytest.raises(RuntimeError):
+        BoboPredicateCallable(call=call)
