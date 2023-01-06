1298e9f465d0562510655098dfdade6252e00094
r3w0p <rr33ww00pp@gmail.com>
2023-01-06 15:30:28 +0000

    Bump version: 0.9.15 → 0.9.16

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="BoboCEP",
-    version="0.9.15",
+    version="0.9.16",
     author="r3w0p",
     author_email="rr33ww00pp@gmail.com",
     description="A fault-tolerant Complex Event Processing engine "
